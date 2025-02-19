c90d7ef309b841e94564b9e72109c9feb505690a
Drekareen <35085025+bear-with-code@users.noreply.github.com>
2025-02-19 20:41:36 -0300

    aula de ontem

---
+++ 18-02/ex1.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+  int ano = 2003;
+  printf("Nascimento: %d", ano);
+  return 0;
+}  
---
+++ 18-02/ex4.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+  char letra = 'Y';
+  printf("Letra: %c\n", letra + 1);
+  printf("Letra: %c\n", letra + 2);
+  printf("Letra: %c\n", letra + 3);
+  printf("Letra: %c\n", letra + 4);
+  printf("Letra: %c\n", letra + 5);
+  printf("Letra: %c\n", letra + 6);
+  printf("Letra: %c\n", letra + 7);
+  printf("Letra: %c\n", letra + 8);
+  printf("Letra: %c\n", letra + 9);
+  printf("Letra: %c", letra + 10);
+  return 0;
+}  
