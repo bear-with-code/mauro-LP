605c5047ca732437a83be6133ad810153c675a1a
Drekareen <35085025+bear-with-code@users.noreply.github.com>
2025-02-18 22:18:56 -0300

    tarefa 18/02

---
+++ 8-2.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+  int ano = 2003, idade; idade = 2026 - ano;
+  printf("Nasci em %d e terei %d no fim do ano que vem", ano, idade);
+  return 0;
+}  
---
+++ 9-3.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+  float usd = 5.685, euro = 5.940, btc = 555688.22;
+  printf("1 USD = R$ %.2f\n1 EUR = R$ %.2f\n1 BTC = R$ %.2f", usd, euro, btc);
+  return 0;
+}  
