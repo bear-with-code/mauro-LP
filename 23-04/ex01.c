7a95a47d5e79fb7518406ef429570217ad648e84
Drekareen <35085025+bear-with-code@users.noreply.github.com>
2025-04-23 19:58:00 -0300

    update

--- 23-04/ex01.c
+++ 23-04/ex01.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
 #include <locale.h>
+#define TAMANHO 5
 
 int main(){
 	setlocale(LC_ALL, "Portuguese");
 	
-	int x[5];
-	x[0] = 1; x[1] = 2; x[2] = 40;
-	x[3] = -8; x[4] = 0;
+	int x[TAMANHO] = {1, 2, 40, -8, 0};
 	
-	for(int i = 0; i < 5; i++){
-		printf("x[%d] = %d", i, x[i]);
-		printf((i < 4) ? "\n" : "");
+	for(int i = 0; i < TAMANHO; i++){
+		printf("x[%d] = %2d", i, x[i]);
+		printf((i < TAMANHO-1) ? "\n" : "");
 	}
 	
 	
